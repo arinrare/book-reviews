18d3cec34ce385cef3a5f01d7551eb20f8854218
mbaggott <5018715+mbaggott@users.noreply.github.com>
2025-08-26 05:54:51 +1000

    Remove default welcome root route. Moview HomeController to root route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', 'App\Http\Controllers\HomeController@index');
+Route::get('/', 'App\Http\Controllers\HomeController@index');

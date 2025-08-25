1fc95374ac8e5298baef21dfe69e9c843c7f6035
mbaggott <5018715+mbaggott@users.noreply.github.com>
2025-08-26 05:12:00 +1000

    Initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $text = "Michael's Book Reviews";
+        return view('home_index', ['text' => $text]);
+    }
+}
+

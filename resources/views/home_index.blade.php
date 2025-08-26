8a11473d76dc8cf987f7e506239243b49a057a2d
mbaggott <5018715+mbaggott@users.noreply.github.com>
2025-08-27 09:59:34 +1000

    Aff favicon. Fix typo on homepage

--- resources/views/home_index.blade.php
+++ resources/views/home_index.blade.php
@@ -3,12 +3,12 @@
 <body class="text-white pt-5 min-h-screen w-full bg-cover bg-center bg-no-repeat" style="background-image: url('/images/fantastic_library.jpg')">
     <div class="font-sans font-bold  text-3xl sm:text-4xl md:text-6xl lg:text-7xl text-center">{{ $text }}</div>
     <div>
-        <div class="text-white pb-20 pt-20 pl-40 pr-40 text-center text-l sm:text-xl md:text-2xl">
-            <p>My name is Michael Baggot, and am an avid fan of Science Fiction and Fantasy novels. These are my favourite genres, though i will sometimes read other genres and the blog will not necessarily be limited to these.</p>
+        <div class="text-white pb-20 pt-40 pl-40 pr-40 text-center text-l sm:text-xl md:text-2xl">
+            <p>My name is Michael Baggott, and am an avid fan of Science Fiction and Fantasy novels. These are my favourite genres, though i will sometimes read other genres and the reviews will not necessarily be limited to these.</p>
             <br/>
-            <p>This blog contains spoiler free reviews, so there is no in depth discussion about plot here. One of my pet hates is reading reviews that spoil a story before you have a chance to read it and draw your own conclusion.</p>
+            <p>This site contains spoiler free reviews, so there is no in depth discussion about plot here. One of my pet hates is reading reviews that spoil a story before you have a chance to read it and draw your own conclusion.</p>
             <br/>
-            <p>Please note the links to purchase books on this site through Amazon, Amazon Australia, and Booktopia are affiliate links, by purchasing through these links you would be supporting the blog and my ability to continue reviewing books.</p>
+            <p>Enjoy the reviews!</p>
         </div>
     </div>
 </body>

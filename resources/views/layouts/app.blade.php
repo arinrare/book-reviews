0e36e57233de1b3aac1ef2fe830d6b5d8ca927d7
mbaggott <5018715+mbaggott@users.noreply.github.com>
2025-09-08 18:22:09 +1000

    added meta tags and banner image for social media sharing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,14 @@
+<!doctype html>
 @vite('resources/css/app.css')
+<head>
+    <meta property="og:title" content="Michael's Book Reviews"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://michaelbaggott.site/bookreviewslaravel/"/>
+    <meta property="og:image" content="https://michaelbaggott.site/bookreviewslaravel/images/banner.jpg"/>
+    <meta property="og:site_name" content="Portfolio of Michael Baggott"/>
+    <meta property="og:description" content="A fantasy/science fiction book review website built in Laravel, that uses Wordpress as a CMS. Designed for my portfolio, but kept up to date with books currently being read by Michael."/>
+</head>
+
 
 <body class="text-white min-h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col" style="background-image: url('{{ asset('images/fantastic_library.jpg') }}')">
     <x-navbar />
